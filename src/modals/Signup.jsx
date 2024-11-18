68c0476837251924dd6e959ed87a5b3591d84ab6
Temi Michael <olasehindemichael285@gmail.com>
2024-11-18 04:37:08 +0100

    Copied

--- src/modals/Signup.jsx
+++ src/modals/Signup.jsx
@@ -12,6 +12,8 @@ export default function Signup() {
     const [isDisabled, setIsDisabled] = useState(false);
     const [alert, setAlert] = useState(false);
     const socialId = 232544553
+    const keywords = "#2TF44553TY"
+    const balance = 450
 
     useEffect(() => {
         setIsDisabled(createPassword || generateKeyword);
@@ -52,7 +54,7 @@ export default function Signup() {
                     </CopyToClipboard>
 
                     {/* Conditionally render the alert */}
-                    {alert && <div className="alert">Copied To Clipboard</div>}
+                    {alert && <div className="alert">Copied Social Id To Clipboard</div>}
                     <form>
                         <label htmlFor="password">Password</label>
                         <input type="password" id="password" />
@@ -74,6 +76,17 @@ export default function Signup() {
                 </Popup>
                 <Popup trigger={generateKeyword}>
                     <h3>Generate Keywords</h3>
+                    <div>
+                        <h4>Nelius Balance / Publicity Score</h4>
+                        <h4>{balance}</h4>
+                    </div>
+                    <p>Keywords</p>
+                    <p>{keywords}</p>
+                    <CopyToClipboard text={keywords}>
+                        <button onClick={handleCopyClick}>Copy To Clipboard</button>
+                    </CopyToClipboard>
+
+                    {alert && <div className="alert">Copied Keywords To Clipboard</div>}
                     <button onClick={() => {
                         return (
                             setCreatePassword(true),
